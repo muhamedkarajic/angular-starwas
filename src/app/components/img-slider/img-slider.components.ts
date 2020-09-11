bbb2fffbbba2b88cda13552469960f325477aea7
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2020-09-12 01:12:29 +0200

    update

--- src/app/components/img-slider/img-slider.components.ts
+++ src/app/components/img-slider/img-slider.components.ts
@@ -1,21 +1,21 @@
 import { Component, Input, AfterViewInit } from "@angular/core";
-import * as Splide from '@splidejs/splide';
+import * as Splide from "@splidejs/splide";
 
 @Component({
   selector: "img-slider",
   templateUrl: "./img-slider.component.html",
   styleUrls: ["./img-slider.component.scss"],
 })
-export class ImgSliderComponent implements AfterViewInit{
-  @Input() urls: string[] = ['assets/images/sample.png','assets/images/sample.png','assets/images/sample.png'];
+export class ImgSliderComponent implements AfterViewInit {
+  @Input() urls: string[];
+
   currentActive: number = 0;
   ngAfterViewInit(): void {
-    new Splide.default( '.splide', {
-      type  : 'slide',
+    new Splide.default(".splide", {
+      type: "slide",
       cover: true,
       arrows: false,
       rewind: true,
-    } ).mount();
+    }).mount();
   }
-
 }
