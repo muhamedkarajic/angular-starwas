446ca9d503fdf55409997717cb018e3ce892a48b
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2020-09-12 00:26:20 +0200

    Update preview-urls.component.ts

--- src/app/components/preview-urls/preview-urls.component.ts
+++ src/app/components/preview-urls/preview-urls.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from "@angular/core";
   styleUrls: ["./preview-urls.component.scss"],
 })
 export class PreviewUrlsComponent implements OnInit {
-  @Input() title: string[] = [];
+  @Input() title: string = null;
   @Input() urls: string[] = [];
   @Input() objects: any[] = [];
 
